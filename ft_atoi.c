7b2fc1552de00b264f3e2daa2118dfa09fa8c3e3
Triin Marts <tmarts@1-C-8.42heilbronn.de>
2022-10-23 23:46:10 +0200

    23 all, only fic stlcat

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,8 +6,42 @@
 /*   By: tmarts <tmarts@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 22:36:55 by tmarts            #+#    #+#             */
-/*   Updated: 2022/10/23 22:37:56 by tmarts           ###   ########.fr       */
+/*   Updated: 2022/10/23 23:14:01 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(const char *str);
+// #include <stdlib.h>
+// #include <stdio.h>
+int	ft_atoi(const char *str)
+{
+	int	intvalue;
+	int	neg_pos;
+
+	intvalue = 0;
+	neg_pos = 1;
+	while ((*str == ' ' || *str == '\f' || *str == '\n'
+			|| *str == '\r' || *str == '\t' || *str == '\v'))
+		str++;
+	if (*str == '-')
+	{
+		neg_pos = -1;
+		str++;
+	}	
+	else if (*str == '+')
+		str++;
+	while (*str >= 48 && *str <= 57)
+	{
+		intvalue = intvalue * 10 + (*str - '0');
+		str++;
+	}
+	return (intvalue * neg_pos);
+}
+
+// int	main(void)
+// {
+// 	char	s1[] = "        -4700llljohdgj";
+// 	char	s2[] = "- 000047000  -608963hjghhj";
+
+// 	printf("%d\n", ft_atoi(s1));
+// 	printf("%d\n", atoi(s2));
+// }
--- ft_strdup.c
+++ ft_strdup.c
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmarts <tmarts@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/23 18:27:42 by tmarts            #+#    #+#             */
-/*   Updated: 2022/10/23 22:47:56 by tmarts           ###   ########.fr       */
+/*   Created: 2022/10/23 23:16:56 by tmarts            #+#    #+#             */
+/*   Updated: 2022/10/23 23:43:16 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+char	*ft_strdup(const char *s1)
 {
-	if (n == 0)
+	char	*s1_cpy;
+	size_t	size;
+
+	size = ft_strlen(s1) + 1;
+	s1_cpy = (char *)malloc(size);
+	if (s1_cpy == 0)
 		return (0);
-	while (n - 1 > 0 && *s1 == *s2 && *s1 != 0 && *s2 != 0)
-	{
-		n--;
-		s1++;
-		s2++;
-	}
-	return ((unsigned char)*s1 - (unsigned char)*s2);
+	ft_strlcpy(s1_cpy, s1, size);
+	return (s1_cpy);
 }
