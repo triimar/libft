c179079c26757cfc7d4ce2438583f7cf87087b14
Triin Marts <tmarts@1-C-10.42heilbronn.de>
2022-10-24 15:08:20 +0200

    24.10 strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,10 @@
 /*   By: tmarts <tmarts@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 14:04:45 by tmarts            #+#    #+#             */
-/*   Updated: 2022/10/24 00:58:59 by tmarts           ###   ########.fr       */
+/*   Updated: 2022/10/24 14:59:40 by tmarts           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <string.h>
-// #include <stdio.h>
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
@@ -19,13 +17,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dstlen;
 	size_t	srclen;
 	size_t	i;
-
-	dstlen = ft_strlen(dst);
+	
 	srclen = ft_strlen(src);
+	if (!dst && dstsize == 0)
+		return (srclen);
+	dstlen = ft_strlen(dst);
 	i = 0;
 	if (dstsize != 0 && dstlen < dstsize)
 	{
-		while (*src != 0 && i < (dstsize - dstlen - 1))
+		while (*(src + i) != 0 && i < (dstsize - dstlen - 1))
 		{
 			*(dst + dstlen + i) = *(src + i);
 			i++;
@@ -35,19 +35,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	}
 	return (srclen + dstsize);
 }
-
-// int	main(void)
-// {
-// 	const char	src[] = "";
-// 	char		dst[8]  = "a";
-// 	const char	osrc[] = "";
-// 	char		odst[8]  = "a";
-
-// 	printf("ft_strlcat result: %zu\n", ft_strlcat(dst, src, 3));
-// 	printf("strlcat result: %zu\n", strlcat(odst, osrc, 3));
-// 	printf("dst after: %s\n", dst);
-// 	printf("odst after: %s\n", odst);
-// 	printf("src after: %s\n", src);
-// 	printf("osrc after: %s\n", osrc);
-// 	return (0);
-// }
